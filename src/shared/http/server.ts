8a68b5e2d8504b60d311d9f3c7b4da606c881808
HugoZAuad <hugozeymer@hotmail.com>
2025-06-03 23:38:55 -0300

    Feat: config init and error trat

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+import 'express-async-errors';
+import ErrorHandleMiddleware from '@shared/middlewares/ErrorHandleMiddleware';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+app.use(ErrorHandleMiddleware.handleError);
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
+})
