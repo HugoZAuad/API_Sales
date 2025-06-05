d9ec6134b2f70f2fd12b827b8b0b77ae799bdc89
HugoZAuad <hugozeymer@hotmail.com>
2025-06-04 22:41:25 -0300

    Feat: find by id of product

---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,18 @@
+import { Product } from './../database/entities/Product';
+import AppError from '@shared/errors/AppError'
+import { productsRepositories } from '../database/repositories/ProductsRepositories'
+interface IShowProduct {
+  id: string;
+}
+
+export default class ShowProductService {
+  async execute({ id }: IShowProduct): Promise<Product>{
+    const product = await productsRepositories.findById(id)
+
+    if(!product){
+      throw new AppError('Produto não encontrado', 404)
+    }
+
+    return product
+  }
+}
