ac0949aeca361080ca105772dda35c0675094111
HugoZAuad <hugozeymer@hotmail.com>
2025-06-05 10:53:06 -0300

    Feat: Schemas product created

--- src/modules/products/routes/ProductRoutes.ts
+++ src/modules/products/routes/ProductRoutes.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
-import ProductsControllers from "../Controllers/ProductsControllers"
+import ProductsControllers from "../controllers/ProductsControllers";
+import {
+  CreateProductSchema,
+  idParamsValidation,
+  UpadateProductSchema,
+} from "../schemas/ProductsSchemas";
 
 const productsRouter = Router();
 const productsController = new ProductsControllers();
 
-productsRouter.get('/', productsController.index);
-productsRouter.get('/:id', productsController.show);
-productsRouter.post('/', productsController.create);
-productsRouter.put('/:id', productsController.update);
-productsRouter.delete('/:id', productsController.delete);
+productsRouter.get("/", productsController.index);
+productsRouter.get("/:id", idParamsValidation, productsController.show);
+productsRouter.post("/", CreateProductSchema, productsController.create);
+productsRouter.put("/:id", UpadateProductSchema, productsController.update);
+productsRouter.delete("/:id", idParamsValidation, productsController.delete);
 
 export default productsRouter;
