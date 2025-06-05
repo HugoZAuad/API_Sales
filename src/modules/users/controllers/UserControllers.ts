132ace988ffd673c8aa9753f02fa84c2e6a65c63
HugoZAuad <hugozeymer@hotmail.com>
2025-06-05 12:09:16 -0300

    feat: user routes

--- src/modules/users/controllers/UserControllers.ts
+++ src/modules/users/controllers/UserControllers.ts
@@ -1,18 +1,18 @@
-import { Request, Response } from 'express'
-import ListUserService from '../services/LiserUserService'
-import CreateUserService from '../services/CreateUserService'
+import { Request, Response } from "express";
+import ListUserService from "../services/LiserUserService";
+import CreateUserService from "../services/CreateUserService";
 
 export default class UsersControllers {
-  async index(request: Request, response: Response): Promise<Response>{
-    const listUsers = new ListUserService()
-    const users = await listUsers.execute()
-    return response.json(users)
+  async index(request: Request, response: Response): Promise<Response> {
+    const listUsers = new ListUserService();
+    const users = await listUsers.execute();
+    return response.json(users);
   }
 
-  async create(request: Request, response: Response): Promise<Response>{
-    const {name, email, password} = request.body;
-    const createUser = new CreateUserService()
-    const user = await createUser.execute({name, email, password})
-    return response.json(user)
+  async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+    const createUser = new CreateUserService();
+    const user = await createUser.execute({ name, email, password });
+    return response.json(user);
   }
 }
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,11 +1,13 @@
-import productsRouter from '@modules/products/routes/ProductRoutes'
-import { Router } from 'express';
+import productsRouter from "@modules/products/routes/ProductRoutes";
+import usersRouter from "@modules/users/routes/UserRoutes";
+import { Router } from "express";
 
 const routes = Router();
 
-routes.get('/health', (request, response) => {
-  return response.json({ message: 'Hello Dev! I am alive!' });
-})
-routes.use('/products', productsRouter)
+routes.get("/health", (request, response) => {
+  return response.json({ message: "Hello Dev! I am alive!" });
+});
+routes.use("/products", productsRouter);
+routes.use("/users", usersRouter);
 
 export default routes;
