fcec8503cf84790c573f338e8885d10c7f0d12d3
HugoZAuad <hugozeymer@hotmail.com>
2025-06-04 22:48:21 -0300

    Feat: Update product Service

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,39 @@
+import AppError from "@shared/errors/AppError";
+import { Product } from "../database/entities/Product";
+import { productsRepositories } from "../database/repositories/ProductsRepositories";
+
+interface IUpdateProductService {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export default class UpdateProductService {
+  async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IUpdateProductService): Promise<Product> {
+    const product = await productsRepositories.findById(id);
+
+    if (!product) {
+      throw new AppError("Produto não encontrado", 404);
+    }
+
+    const productExists = await productsRepositories.findByName(name);
+
+    if (productExists) {
+      throw new AppError("Já existe um produto com esse nome", 409);
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepositories.save(product)
+
+    return product;
+  }
+}
